469bc94b4a3736343fb754c55efcaaa7ba0decab
Douglas Leal <cac.diretoria@cace.asp.org.br>
2023-07-27 19:28:01 -0300

    upd diversas escalas

--- dbox.js
+++ dbox.js
@@ -95,49 +95,8 @@ if ( r < 86400000 ) {
 
 // Plantão sexta
 let plantaosexta = [
-    {data: new Date(2023, 1, 27), person: "Douglas"},
-    {data: new Date(2023, 2, 3), person: "Thiago"},
-    {data: new Date(2023, 2, 10), person: "Rudi"},
-    {data: new Date(2023, 2, 17), person: "Sandra"},
-    {data: new Date(2023, 2, 24), person: "Raquel"},
-    {data: new Date(2023, 2, 31), person: "Guilherme"},
-    {data: new Date(2023, 3, 7), person: "Feriado"},
-    {data: new Date(2023, 3, 14), person: "Cida"},
-    {data: new Date(2023, 3, 21), person: "Feriado"},
-    {data: new Date(2023, 3, 28), person: "Karol"},
-    {data: new Date(2023, 4, 5), person: "Jessica"},
-    {data: new Date(2023, 4, 12), person: "Eduardo"},
-    {data: new Date(2023, 4, 19), person: "Denise"},
-    {data: new Date(2023, 4, 26), person: "Eliesio"},
-    {data: new Date(2023, 5, 2), person: "Roberta"},
-    {data: new Date(2023, 5, 9), person: "Recesso"},
-    {data: new Date(2023, 5, 16), person: "Denise"},
-    {data: new Date(2023, 5, 23), person: "Guilherme"},
-    {data: new Date(2023, 5, 30), person: "Thiago"},
-    {data: new Date(2023, 5, 30), person: "Rudi"},
-    {data: new Date(2023, 6, 7), person: "Thiago (vice)"},
-    {data: new Date(2023, 6, 14), person: "Recesso"},
-    {data: new Date(2023, 6, 21), person: "Recesso"},
-    {data: new Date(2023, 6, 28), person: "Thiago"},
-    {data: new Date(2023, 7, 4), person: "Léo"},
-    {data: new Date(2023, 7, 11), person: "Eliesio"},
-    {data: new Date(2023, 7, 18), person: "Mirian"},
-    {data: new Date(2023, 7, 25), person: ""},
-    {data: new Date(2023, 8, 1), person: ""},
-    {data: new Date(2023, 8, 8), person: ""},
-    {data: new Date(2023, 8, 15), person: ""},
-    {data: new Date(2023, 8, 22), person: ""},
-    {data: new Date(2023, 8, 29), person: ""},
-    {data: new Date(2023, 9, 6), person: ""},
-    {data: new Date(2023, 9, 13), person: ""},
-    {data: new Date(2023, 9, 20), person: ""},
-    {data: new Date(2023, 9, 27), person: ""},
-    {data: new Date(2023, 10, 3), person: ""},
-    {data: new Date(2023, 10, 10), person: ""},
-    {data: new Date(2023, 10, 17), person: ""},
-    {data: new Date(2023, 10, 24), person: ""},
-    {data: new Date(2023, 11, 1), person: ""},
-    {data: new Date(2023, 11, 8), person: ""},
+    {data: new Date(2023, 7, 28), person: "Douglas"},
+    
 ]
 for(j=0; j<plantaosexta.length; j++) {
 
@@ -152,167 +111,82 @@ if ( diffDays >= -6 && diffDays < 1 ) {
 
 
 // Cultos
-let cultos = [
-    {data: new Date(2023, 6, 28), pessoaM: '', pessoaT: '', dletivo: true},
-    {data: new Date(2023, 6, 29), pessoaM: 'Sábado', pessoaT: 'Sábado', dletivo: false},
-    {data: new Date(2023, 6, 30), pessoaM: 'Domingo', pessoaT: 'Domingo', dletivo: false},
-    {data: new Date(2023, 6, 31), pessoaM: '', pessoaT: '', dletivo: true},
-]
-for( k=0; k<cultos.length; k++ ) {
+window.addEventListener('load', cultos)
+function cultos() {
 
     if (datahoje.getDay() == 0) {
-        document.querySelector('#cultos').innerHTML = "HOje é domingo!"
+        document.querySelector('#cultos').innerHTML = "Hoje é domingão!"
     }
-
     if (datahoje.getDay() == 1) {
-        document.querySelector('#cultos').innerHTML = 'Hoje é domingo.'
+        document.querySelector('#cultos').innerHTML = '7h Diretor | 10h10-Diretor | 13h-Diretor'
     }
-
     if (datahoje.getDay() == 2) {
-        document.querySelector('#cultos').innerHTML = 'Hoje é domingo.'
+        document.querySelector('#cultos').innerHTML = '7h Pr. Vinícius | 10h10 Pr. Vinícius | 13h Pr. Vinícius'
     }
     if (datahoje.getDay() == 3) {
-        document.querySelector('#cultos').innerHTML = 'Hoje é domingo.'
+        document.querySelector('#cultos').innerHTML = '7h Vice-diretor | 10h10 Vice-diretor | 13h Vice-diretor'
     }
     if (datahoje.getDay() == 4) {
-        document.querySelector('#cultos').innerHTML = 'Hoje é domingo.'
+        if ( datahoje.getDate() == 3) {
+            document.querySelector('#cultos').innerHTML = '7h Prof. Eula | 10h10 Prof. Nailson | 13h Prof. Jaqueline' 
+        }
+        if ( datahoje.getDate() == 10) {
+            document.querySelector('#cultos').innerHTML = '7h Prof. Carol | 10h10 Prof. Malú | 13h Prof. Valéria' 
+        }
+        if ( datahoje.getDate() == 17) {
+            document.querySelector('#cultos').innerHTML = '7h Prof. Filipe | 10h10 Sec. Katleen | 13h Prof. Prof. Allan' 
+        }
+        if ( datahoje.getDate() == 24) {
+            document.querySelector('#cultos').innerHTML = '7h Prof. Noemi | 10h10 Prof. Sarita | 13h Prof. Eliane' 
+        }
+        if ( datahoje.getDate() == 31) {
+            document.querySelector('#cultos').innerHTML = '7h Prof. Fernando | 10h10 Prof. Camila | 13h Prof. Drika' 
+        }
+        
     }
-    if (datahoje.getDay() == 1) {
-        document.querySelector('#cultos').innerHTML = 'Hoje é domingo.'
+    if (datahoje.getDay() == 5) {
+        document.querySelector('#cultos').innerHTML = ''
+    }
+    if (datahoje.getDay() == 6) {
+        document.querySelector('#cultos').innerHTML = 'Feliz Sábado!'
     }
 
-    // if ( datahoje.getDay() == cultos[k].data.getDay() && cultos[k].dletivo == false ) {
-    //     document.querySelector('#cultos').innerHTML = 'Recesso/Feriado'
-    // }
-
-    // if ( (datahoje.getDate() === cultos[k].data.getDate()) && (datahoje.getMonth() === cultos[k].data.getMonth()) && (datahoje.getFullYear() === cultos[k].data.getFullYear()) ){
-    //     document.querySelector('#titulocultos').innerHTML = "Cultos " + cultos[k].data.getDate() + '/' + (cultos[k].data.getMonth()+1)
-    //     document.querySelector('#cultos').innerHTML = cultos[k].pessoaM + ' e ' + cultos[k].pessoaT
-    // }
 }
 
-
+datahoje = new Date(2023, 7, 1)
 //Plantão Diário
-let plantaodiario = [
+window.addEventListener('load', plantaoDiarioCompensacao)
+function plantaoDiarioCompensacao() {
 
-    {data: new Date(2023, 2, 13), pessoa: "Diretor"},
-    {data: new Date(2023, 2, 14), pessoa: "Rudi"},
-    {data: new Date(2023, 2, 15), pessoa: "Diretor"},
-    {data: new Date(2023, 2, 16), pessoa: "Thiago"},
-
-    {data: new Date(2023, 2, 20), pessoa: "Diretor"},
-    {data: new Date(2023, 2, 21), pessoa: "Rudi"},
-    {data: new Date(2023, 2, 22), pessoa: "Rudi"},
-    {data: new Date(2023, 2, 23), pessoa: "Thiago"},
-
-    {data: new Date(2023, 2, 27), pessoa: "Diretor"},
-    {data: new Date(2023, 2, 28), pessoa: "Rudi"},
-    {data: new Date(2023, 2, 29), pessoa: "Thiago"},
-    {data: new Date(2023, 2, 30), pessoa: "Thiago"},
-
-    {data: new Date(2023, 3, 3), pessoa: "Diretor"},
-    {data: new Date(2023, 3, 4), pessoa: "Rudi"},
-    {data: new Date(2023, 3, 5), pessoa: "Thiago"},
-    {data: new Date(2023, 3, 6), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 3, 10), pessoa: "Diretor"},
-    {data: new Date(2023, 3, 11), pessoa: "Rudi"},
-    {data: new Date(2023, 3, 12), pessoa: "Thiago"},
-    {data: new Date(2023, 3, 13), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 3, 17), pessoa: "Diretor"},
-    {data: new Date(2023, 3, 18), pessoa: "Rudi"},
-    {data: new Date(2023, 3, 19), pessoa: "Thiago"},
-    {data: new Date(2023, 3, 20), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 3, 24), pessoa: "Diretor"},
-    {data: new Date(2023, 3, 25), pessoa: "Rudi"},
-    {data: new Date(2023, 3, 26), pessoa: "Thiago"},
-    {data: new Date(2023, 3, 27), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 4, 2), pessoa: "Rudi"},
-    {data: new Date(2023, 4, 3), pessoa: "Thiago"},
-    {data: new Date(2023, 4, 4), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 4, 8), pessoa: "Diretor"},
-    {data: new Date(2023, 4, 9), pessoa: "Rudi"},
-    {data: new Date(2023, 4, 10), pessoa: "Thiago"},
-    {data: new Date(2023, 4, 11), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 4, 15), pessoa: "Diretor"},
-    {data: new Date(2023, 4, 16), pessoa: "Rudi"},
-    {data: new Date(2023, 4, 17), pessoa: "Thiago"},
-    {data: new Date(2023, 4, 18), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 4, 22), pessoa: "Diretor"},
-    {data: new Date(2023, 4, 23), pessoa: "Rudi"},
-    {data: new Date(2023, 4, 24), pessoa: "Thiago"},
-    {data: new Date(2023, 4, 25), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 4, 29), pessoa: "Diretor"},
-    {data: new Date(2023, 4, 30), pessoa: "Rudi"},
-    {data: new Date(2023, 4, 31), pessoa: "Thiago"},
-    {data: new Date(2023, 5, 1), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 5, 5), pessoa: "Diretor"},
-    {data: new Date(2023, 5, 6), pessoa: "Rudi"},
-    {data: new Date(2023, 5, 7), pessoa: "Thiago"},
-    {data: new Date(2023, 5, 8), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 5, 12), pessoa: "Diretor"},
-    {data: new Date(2023, 5, 13), pessoa: "Rudi"},
-    {data: new Date(2023, 5, 14), pessoa: "Thiago"},
-    {data: new Date(2023, 5, 15), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 5, 19), pessoa: "Diretor"},
-    {data: new Date(2023, 5, 20), pessoa: "Rudi"},
-    {data: new Date(2023, 5, 21), pessoa: "Thiago"},
-    {data: new Date(2023, 5, 22), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 5, 26), pessoa: "Diretor"},
-    {data: new Date(2023, 5, 27), pessoa: "Rudi"},
-    {data: new Date(2023, 5, 28), pessoa: "Diretor"},
-    {data: new Date(2023, 5, 29), pessoa: "Thiago"},
-
-    {data: new Date(2023, 6, 3), pessoa: "Diretor"},
-    {data: new Date(2023, 6, 4), pessoa: "Rudi"},
-    {data: new Date(2023, 6, 5), pessoa: "Thiago"},
-    {data: new Date(2023, 6, 6), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 6, 10), pessoa: "Recesso"},
-    {data: new Date(2023, 6, 11), pessoa: "Recesso"},
-    {data: new Date(2023, 6, 12), pessoa: "Recesso"},
-    {data: new Date(2023, 6, 13), pessoa: "Recesso"},
-
-    {data: new Date(2023, 6, 17), pessoa: "Recesso"},
-    {data: new Date(2023, 6, 18), pessoa: "Recesso"},
-    {data: new Date(2023, 6, 19), pessoa: "Recesso"},
-    {data: new Date(2023, 6, 20), pessoa: "Recesso"},
-
-    {data: new Date(2023, 6, 24), pessoa: "Recesso"},
-    {data: new Date(2023, 6, 25), pessoa: "Rudi"},
-    {data: new Date(2023, 6, 26), pessoa: "Thiago"},
-    {data: new Date(2023, 6, 27), pessoa: "Pr. Guilherme"},
-
-    {data: new Date(2023, 6, 31), pessoa: "Diretor"},
-    {data: new Date(2023, 6, 1), pessoa: "Rudi"},
-    {data: new Date(2023, 6, 2), pessoa: "Thiago"},
-    {data: new Date(2023, 6, 3), pessoa: "Pr. Guilherme"},
-
-]
-for (let m = 0; m < plantaodiario.length; m++ ) {
-
-    if ( (datahoje.getDate() === plantaodiario[m].data.getDate() ) && (datahoje.getMonth() === plantaodiario[m].data.getMonth()) && (datahoje.getFullYear() === plantaodiario[m].data.getFullYear()) ) {  
-        document.querySelector('#plantaodiario').innerHTML = plantaodiario[m].data.getDate() + '/' + (plantaodiario[m].data.getMonth()+1) + ' - ' + plantaodiario[m].pessoa
-    } else if (datahoje.getDay() == 5) {
-        document.querySelector('#plantaodiario').innerHTML = 'Ver plantão de sexta'
-    } else if (datahoje.getDay() == 6) {
-        document.querySelector('#plantaodiario').innerHTML = 'Hoje é sábado!'
-    } else if (datahoje.getDay() == 0) {
-        document.querySelector('#plantaodiario').innerHTML = 'Hoje é domingo!'
+    if (datahoje.getDay() == 0) { 
+        document.querySelector('#plantaodiario').innerHTML = 'Hoje é domingão!'
+        document.querySelector('#nomescomp').innerHTML = 'Hoje é domingão!'
+    }    
+    if (datahoje.getDay() == 1) { 
+        document.querySelector('#plantaodiario').innerHTML = 'Diretor'
+        document.querySelector('#nomescomp').innerHTML = 'Manhã: Pr. Vinícius'
+    }
+    if (datahoje.getDay() == 2) { 
+        document.querySelector('#plantaodiario').innerHTML = 'Vice-diretor'
+        document.querySelector('#nomescomp').innerHTML = 'Sem escala'
+    }
+    if (datahoje.getDay() == 3) { 
+        document.querySelector('#plantaodiario').innerHTML = 'Tesoureira'
+        document.querySelector('#nomescomp').innerHTML = 'Manhã: Jaqueline/Camila - Tarde: Katleen/Nailson'
+    }
+    if (datahoje.getDay() == 4) { 
+        document.querySelector('#plantaodiario').innerHTML = 'Coord. Disciplinar'
+        document.querySelector('#nomescomp').innerHTML = 'Tarde: Valéria/Eula'
+    }
+    if (datahoje.getDay() == 5) { 
+        document.querySelector('#plantaodiario').innerHTML = 'Ver plantão de sexta.'
+        document.querySelector('#nomescomp').innerHTML = 'Manhã: Malú/Douglas - Tarde: Josi/Ale/Carol/Sarita'
+    }
+    if (datahoje.getDay() == 6) { 
+        document.querySelector('#plantaodiario').innerHTML = 'Feliz Sábado!'
+        document.querySelector('#nomescomp').innerHTML = 'Feliz Sábado!'
     }
 }
-
 
 // Modal urgências
 let modalurgencias = document.querySelector('#fonesurgencias')
@@ -421,17 +295,10 @@ function getCurrentEvent() {
     }
 }
 
-
 // ESCALAS
-
 const divescalas = document.querySelectorAll('.escala')
-
 const escalas = Array.from(divescalas)
-
 const ultimaescala = escalas.length - 1
-
-// console.log(ultimaescala)
-
 function arredondarBordasInferiores() {
     const uesc = escalas[ultimaescala]
     uesc.style.borderRadius = "0 0 12px 12px"
@@ -448,9 +315,7 @@ let route = [
     {data: new Date(2023, 10, 25), tema: 'Celebração Route', convidado: ''},
     {data: new Date(2023, 11, 2), tema: 'Celebração final', convidado: ''},
 ]
-
-window.onload(findNearestDate(route))
-
+window.addEventListener('load', findNearestDate(route))
 function findNearestDate(array) {
     // Obter a data atual
     const currentDate = new Date();
@@ -468,14 +333,3 @@ function findNearestDate(array) {
     // Caso contrário, retorna a última data no array (mais distante do dia de hoje)
     // return array[array.length - 1];
   }
-  
-//   const nearestDate = findNearestDate(route);
-  // Output será o objeto cuja data é igual ou a mais próxima data posterior ao dia de hoje
-
-// document.querySelector('#routescala').innerHTML =  nearestDate()
-
-
-
-
-
-        
