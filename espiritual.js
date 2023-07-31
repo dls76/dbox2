5af94b61d0c0b202e1e0cc8ddbf57c00dff5c94b
Douglas <75459253+dls76@users.noreply.github.com>
2023-07-30 21:22:21 -0300

    upd espiritual e add font cera

--- espiritual.js
+++ espiritual.js
@@ -1,22 +1,22 @@
 
-// let openAllLinks = document.querySelector('#btAbrirLinks')
-// openAllLinks.addEventListener('click', abrirLinks)
+let openAllLinks = document.querySelector('#btAbrirLinks')
+openAllLinks.addEventListener('click', abrirLinks)
 
-// let pedidos = document.querySelector('#pedidosOracao')
-// pedidos.addEventListener('click', pedidosOracao)
+let pedidos = document.querySelector('#pedidosOracao')
+pedidos.addEventListener('click', pedidosOracao)
 
-// function abrirLinks() {
-//     var sites = [
-//         'https://mais.cpb.com.br/licao-adultos/',
-//         'https://mais.cpb.com.br/meditacoes-diarias/',
-//         'https://www.bibliaonline.com.br/naa/index',
-//         'https://reavivadosporsuapalavra.org/', 
-//     ] 
-//     for (let i = 0; i < sites.length; i++) {
-//         window.open(sites[i])
-//     }
-// };
+function abrirLinks() {
+    var sites = [
+        'https://mais.cpb.com.br/licao-adultos/',
+        'https://mais.cpb.com.br/meditacoes-diarias/',
+        'https://www.bibliaonline.com.br/',
+        'https://reavivadosporsuapalavra.org/', 
+    ] 
+    for (let i = 0; i < sites.length; i++) {
+        window.open(sites[i])
+    }
+};
 
-// function pedidosOracao() {
-//     alert('Não disponível no momento')
-// }
+function pedidosOracao() {
+    alert('Não disponível no momento')
+}
