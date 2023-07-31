ad0ca619670745691ff2b64928c659fb541f2403
Douglas <75459253+dls76@users.noreply.github.com>
2023-07-30 23:17:49 -0300

    upd aniversariantes julho

--- aniversariantes.js
+++ aniversariantes.js
@@ -6,9 +6,18 @@ const mss = [
 
 const pessoas = [
     
-    //JULHO
-    
-    {nome: "Monica Cristina Martins de Novais", dia: 31, mes: 6, ano: 1979, funcao: "Função", foto:"img/user.png", nascimento: new Date(1979, 6, 31)},
+    //JULHO	
+    {nome: "Ana Prado Bevilaqua", dia: 4, mes: 6, ano: 1978, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Daniela Regina Lamarques Isobe", dia: 4, mes: 6, ano: 1975, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Noemi Macedo Amorim", dia: 7, mes: 6, ano: 1976, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Cacilda Smaha Mannala", dia: 16, mes: 6, ano: 1972, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Ana Amelia Loureço Simas", dia: 21, mes: 6, ano: 1978, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Egislaine Regina Gonsaga", dia: 22, mes: 6, ano: 1978, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Nailson Veras do Nascimento", dia: 24, mes: 6, ano: 1979, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Thayse Oliveira dos Passos", dia: 25, mes: 6, ano: 1984, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Eloiza Carla dos Santos", dia: 27, mes: 6, ano: 2002, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Leidiane Cristina Germano", dia: 27, mes: 6, ano: 1988, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Monica Cristina Martins de Novais", dia: 31, mes: 6, ano: 1979, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
     
     //AGOSTO
     {nome: "Vanessa Moura de Freitas", dia: 2, mes: 7, ano: 1985, funcao: "Função", foto:"", nascimento: new Date()},
