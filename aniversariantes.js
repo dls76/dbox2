c0e08c09899ace95a3b879f24395d9ac0501b459
Douglas <75459253+dls76@users.noreply.github.com>
2023-08-01 23:11:40 -0300

    upd aniversariantes-list

--- aniversariantes.js
+++ aniversariantes.js
@@ -7,26 +7,26 @@ const mss = [
 const pessoas = [
     
     //JULHO	
-    {nome: "Ana Prado Bevilaqua", dia: 4, mes: 6, ano: 1978, funcao: "Professor(a)", foto:"img/anivImg/julho/anaprado.jpg", nascimento: new Date()},
-    {nome: "Daniela Regina Lamarques Isobe", dia: 4, mes: 6, ano: 1975, funcao: "Coordenadora", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Noemi Macedo Amorim", dia: 7, mes: 6, ano: 1976, funcao: "Professor(a)", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Cacilda Smaha Mannala", dia: 16, mes: 6, ano: 1972, funcao: "Bibliotecaria", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Ana Amelia Loureço Simas", dia: 21, mes: 6, ano: 1978, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Egislaine Regina Gonsaga", dia: 22, mes: 6, ano: 1978, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Nailson Veras do Nascimento", dia: 24, mes: 6, ano: 1979, funcao: "Coordenador(a) Disciplinar", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Thayse Oliveira dos Passos", dia: 25, mes: 6, ano: 1984, funcao: "Auxiliar de Biblioteca", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Eloiza Carla dos Santos", dia: 27, mes: 6, ano: 2002, funcao: "Auxiliar de Secretaria", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Leidiane Cristina Germano", dia: 27, mes: 6, ano: 1988, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
-    {nome: "Monica Cristina Martins de Novais", dia: 31, mes: 6, ano: 1979, funcao: "Função", foto:"img/user.png", nascimento: new Date()},
+    {nome: "Ana Prado Bevilaqua", dia: 4, mes: 6, ano: 1978, funcao: "Professor(a)", foto:"img/avatar.png", nascimento: new Date()},
+    {nome: "Daniela Regina Lamarques Isobe", dia: 4, mes: 6, ano: 1975, funcao: "Coordenadora", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Noemi Macedo Amorim", dia: 7, mes: 6, ano: 1976, funcao: "Professor(a)", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Cacilda Smaha Mannala", dia: 16, mes: 6, ano: 1972, funcao: "Bibliotecaria", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Ana Amelia Loureço Simas", dia: 21, mes: 6, ano: 1978, funcao: "Função", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Egislaine Regina Gonsaga", dia: 22, mes: 6, ano: 1978, funcao: "Função", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Nailson Veras do Nascimento", dia: 24, mes: 6, ano: 1979, funcao: "Coordenador(a) Disciplinar", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Thayse Oliveira dos Passos", dia: 25, mes: 6, ano: 1984, funcao: "Auxiliar de Biblioteca", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Eloiza Carla dos Santos", dia: 27, mes: 6, ano: 2002, funcao: "Auxiliar de Secretaria", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Leidiane Cristina Germano", dia: 27, mes: 6, ano: 1988, funcao: "Função", foto:"img/account.png", nascimento: new Date()},
+    {nome: "Monica Cristina Martins de Novais", dia: 31, mes: 6, ano: 1979, funcao: "Função", foto:"img/account.png", nascimento: new Date()},
     
     //AGOSTO
-    {nome: "Vanessa Moura de Freitas", dia: 2, mes: 7, ano: 1985, funcao: "Função", foto:"", nascimento: new Date()},
-    {nome: "Caroline Cristina de Brito", dia: 4, mes: 7, ano: 1988, funcao: "Função", foto:"", nascimento: new Date()},
-    {nome: "Elizabeth Alves Braun Soares", dia: 6, mes: 7, ano: 1971, funcao: "Função", foto:"", nascimento: new Date()},
-    {nome: "Leonardo Ormianin Torres", dia: 12, mes: 7, ano: 1999, funcao: "Função", foto:"", nascimento: new Date()},
-    {nome: "Jessica Aparecida Dionisio", dia: 12, mes: 7, ano: 1996, funcao: "Função", foto:"", nascimento: new Date()},
-    {nome: "Allan Almeida Lima", dia: 26, mes: 7, ano: 1982, funcao: "Função", foto:"", nascimento: new Date()},
-    {nome: "Laís Daniele dos Santos", dia: 28, mes: 7, ano: 2001, funcao: "Função", foto:"", nascimento: new Date()},
+    {nome: "Vanessa Moura de Freitas", dia: 2, mes: 7, ano: 1985, funcao: "Função", foto:"img/user2.png", nascimento: new Date()},
+    {nome: "Caroline Cristina de Brito", dia: 4, mes: 7, ano: 1988, funcao: "Função", foto:"img/user2.png", nascimento: new Date()},
+    {nome: "Elizabeth Alves Braun Soares", dia: 6, mes: 7, ano: 1971, funcao: "Função", foto:"img/user2.png", nascimento: new Date()},
+    {nome: "Leonardo Ormianin Torres", dia: 12, mes: 7, ano: 1999, funcao: "Função", foto:"img/user2.png", nascimento: new Date()},
+    {nome: "Jessica Aparecida Dionisio", dia: 12, mes: 7, ano: 1996, funcao: "Função", foto:"img/user2.png", nascimento: new Date()},
+    {nome: "Allan Almeida Lima", dia: 26, mes: 7, ano: 1982, funcao: "Função", foto:"img/user2.png", nascimento: new Date()},
+    {nome: "Laís Daniele dos Santos", dia: 28, mes: 7, ano: 2001, funcao: "Função", foto:"img/user2.png", nascimento: new Date()},
     
     //SETEMBRO
     {nome: "Renata Brasil Lima", dia: 1, mes: 8, ano: 1993, funcao: "Função", foto:"", nascimento: new Date()},
@@ -36,15 +36,6 @@ const pessoas = [
     {nome: "Rafaela Lourenco de Mello", dia: 25, mes: 8, ano: 1990, funcao: "Função", foto:"", nascimento: new Date()},
     {nome: "Daniel Faria Filho", dia: 26, mes: 8, ano: 1963, funcao: "Função", foto:"", nascimento: new Date()},
     {nome: "Vinicius Fernando Serain", dia: 27, mes: 8, ano: 1990, funcao: "Pastoral", foto:"", nascimento: new Date()},
-
-    //OUTUBRO
-
-    
-    //NOVEMBRO
-
-    
-    //DEZEMBRO
-
     
 ]
 
