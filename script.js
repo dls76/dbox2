03d9a64e5000535633029685083eafade5426bee
Douglas <75459253+dls76@users.noreply.github.com>
2023-09-20 22:25:06 -0300

    upd livros didáticos
    add diferença didáticos

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click', toggleMenu)
+
+
+let pessoa = {nome: 'Matheus', idade: 29, cidade: 'Floripa'}
+
+// Transformar o objeto em string e salvar em localStorage
+localStorage.setItem('pessoa', JSON.stringify(pessoa));
+
+// Receber a string
+let pessoaString = localStorage.getItem('pessoa');
+
+// transformar em objeto novamente
+let pessoaObj = JSON.parse(pessoaString);
+
+console.log(pessoaObj.nome + " " + pessoaObj.idade); // Matheus
