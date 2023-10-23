3bb73204c50873afa6a9c1517c7f77d56d4769d5
Douglas <75459253+dls76@users.noreply.github.com>
2023-10-22 21:08:05 -0300

    upd proximos eventos
    criadas novas classes e melhorada a estilização

--- script.js
+++ script.js
@@ -19,61 +19,67 @@ btnMobile.addEventListener('click', toggleMenu)
 //     document.getElementById('nextEventConteiner').scrollTop = topPos;
 // }
 
-const proximoseventos = [
-    {dataInicio: new Date(2023, 9, 20), dataFim: new Date(2023, 9, 20), nome: 'Teste 1', assignee: 'Douglas', pic: 'img/fotos/douglas.jpg'},
-    {dataInicio: new Date(2023, 9, 21), dataFim: new Date(2023, 9, 21), nome: 'Teste 2', assignee: 'Douglas', pic: 'img/fotos/douglas.jpg'},
-    {dataInicio: new Date(2023, 9, 22), dataFim: new Date(2023, 9, 22), nome: 'Teste 3', assignee: 'Douglas', pic: 'img/fotos/douglas.jpg'},
-    {dataInicio: new Date(2023, 9, 23), dataFim: new Date(2023, 9, 23), nome: 'Teste 4', assignee: 'Douglas', pic: 'img/fotos/douglas.jpg'},
-    {dataInicio: new Date(2023, 9, 24), dataFim: new Date(2023, 9, 24), nome: 'Teste 5', assignee: 'Douglas', pic: 'img/fotos/douglas.jpg'},
-    {dataInicio: new Date(2023, 9, 25), dataFim: new Date(2023, 9, 25), nome: 'Teste 6', assignee: 'Douglas', pic: 'img/fotos/douglas.jpg'},
-    {dataInicio: new Date(2023, 9, 26), dataFim: new Date(2023, 9, 29), nome: 'Teste 7', assignee: 'Douglas', pic: 'img/fotos/douglas.jpg'},
-]
-
-
+// const proximoseventos = [
+//     {dataInicio: new Date(2023, 9, 20), dataFim: new Date(2023, 9, 20), nome: 'Teste 1', assignee: 'DLS', pic: 'img/dls.png'},
+//     {dataInicio: new Date(2023, 9, 21), dataFim: new Date(2023, 9, 21), nome: 'Teste 2', assignee: 'DLS', pic: 'img/dls.png'},
+//     {dataInicio: new Date(2023, 9, 22), dataFim: new Date(2023, 9, 22), nome: 'Teste 3', assignee: 'DLS', pic: 'img/dls.png'},
+//     {dataInicio: new Date(2023, 9, 23), dataFim: new Date(2023, 9, 23), nome: 'Teste 4', assignee: 'DLS', pic: 'img/dls.png'},
+//     {dataInicio: new Date(2023, 9, 24), dataFim: new Date(2023, 9, 24), nome: 'Teste 5', assignee: 'DLS', pic: 'img/dls.png'},
+//     {dataInicio: new Date(2023, 9, 25), dataFim: new Date(2023, 9, 25), nome: 'Teste 6', assignee: 'DLS', pic: 'img/dls.png'},
+//     {dataInicio: new Date(2023, 9, 26), dataFim: new Date(2023, 9, 29), nome: 'Teste 7', assignee: 'DLS', pic: 'img/dls.png'},
+// ]
+/*
 for ( let i=0; i<proximoseventos.length; i++ ) {
 
     let conteinerNextEvents = document.querySelector('#nextEventConteiner')
+    
+    // Se as datas de início e fim forem diferentes
+    if (true) { 
 
-    // Append to parents
-    // chamar a função de scrollar o alvo para o topo 
-    
-    // Cria as divs
-    let dataBox = document.createElement('div')
-    let beginningDay = document.createElement('div')
-    let closeDay = document.createElement('div')
-    let mes = document.createElement('div')
-    // let info = document.createElement('div')
-    let eventName = document.createElement('div')
-    // let orcamento = document.createElement('a')
-    // let programa = document.createElement('a')
-    let assignee = document.createElement('div')
-    
-    // Adiciona as classes e IDs nas divs
-    dataBox.classList.add('dataBox')
-    beginningDay.classList.add('beginningDay')
-    closeDay.classList.add('closeDay')
-    mes.classList.add('mes')
-    // info.classList.add('info')
-    eventName.classList.add('eventName')
-    // orcamento.classList.add('orcamento')
-    // programa.classList.add('programa')
-    assignee.classList.add('assignee')   
-    
-    // Insere conteúdo nas divs
-    beginningDay.innerHTML = proximoseventos[i].dataInicio.getDate()
-    closeDay.innerHTML = proximoseventos[i].dataFim.getDate()
-    mes.innerHTML = proximoseventos[i].dataFim.getMonth() + 1
-    eventName.innerHTML = proximoseventos[i].nome
-    assignee.innerHTML = proximoseventos[i].assignee
-    
-    // Append todos os elementos em seus parents
-    dataBox.appendChild(beginningDay)
-    dataBox.appendChild(closeDay)
-    dataBox.appendChild(mes)
-    dataBox.appendChild(eventName)
-    dataBox.appendChild(assignee)
-    // conteinerNextEvents.appendChild(dataBox)
-}
+        
+        let nextEvBox = document.createElement('div')
+        let dataBox = document.createElement('div')
+        let diasBox = document.createElement('div')
+        let beginningDay = document.createElement('div')
+        let closeDay = document.createElement('div')
+        let mes = document.createElement('div')
+        let eventInfo = document.createElement('div')
+        let eventName = document.createElement('div')
+        let assignee = document.createElement('div')
+        let imgAssignee = new Image()
+        imgAssignee.src = proximoseventos[i].pic
+         
+        nextEvBox.classList.add('nextEvBox')
+        dataBox.classList.add('dataBox')
+        diasBox.classList.add('diasBox')
+        eventInfo.classList.add('eventInfo')
+        beginningDay.classList.add('beginningDay')
+        closeDay.classList.add('closeDay')
+        mes.classList.add('mes')
+        eventName.classList.add('eventName')
+        assignee.classList.add('assignee')
+        imgAssignee.classList.add('imgAssignee')   
+        
+        beginningDay.innerHTML = proximoseventos[i].dataInicio.getDate()
+        closeDay.innerHTML = proximoseventos[i].dataFim.getDate()
+        mes.innerHTML = proximoseventos[i].dataFim.getMonth() + 1
+        eventName.innerHTML = proximoseventos[i].nome
+        
+        assignee.appendChild(imgAssignee)
+        dataBox.appendChild(diasBox)
+        diasBox.appendChild(beginningDay)
+        diasBox.appendChild(closeDay)
+        dataBox.appendChild(mes)
+        eventInfo.appendChild(eventName)
+        nextEvBox.appendChild(dataBox)
+        nextEvBox.appendChild(eventInfo)
+        nextEvBox.appendChild(assignee)
+        conteinerNextEvents.appendChild(nextEvBox)
+
+    } else {
+        console.log('else')
+    }
+}*/
 
 //funcao que identifica o alvo, determina seu offsettop e scrolla o conteiner do alvo até o offsettop do alvo:
 // Verificar quais datas do array proximosevetnos sao iguais ou superiores a data atual
@@ -87,11 +93,10 @@ for ( let i=0; i<proximoseventos.length; i++ ) {
 
 
 
-// Copiar link para página de cadastro de currículos do RH
 
+// Copia link da página de cadastro de currículos do RH para a clipboard
 let btcurr = document.querySelector('#btcurriculos')
 btcurr.addEventListener('click', copiarlinkcurriculos)
-
 function copiarlinkcurriculos() {
     let textoCopiado = document.getElementById("inputcurriculos");
     textoCopiado.select();
