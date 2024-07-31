6182fed0020a775da35a83c6a21b1012024478e1
Douglas <75459253+dls76@users.noreply.github.com>
2024-07-31 20:34:38 -0300

    upd celebra EI-5º
    add segundo semestre

--- celebrasInf.js
+++ celebrasInf.js
@@ -3,19 +3,47 @@ window.addEventListener('load', carregarCelebras)
 function carregarCelebras() {
 
     const celebras = [
-        {data: new Date(2024, 4, 9), responsavel: 'Pastoral', tema: 'Família - Dia das Mães | Fantoche com a temática: Família Buscapé.'},
-        {data: new Date(2024, 4, 16), responsavel: 'SOE e SOP', tema: 'Mérito Acadêmico | Projeto de Vida.'},
-        {data: new Date(2024, 4, 23), responsavel: 'Pastoral', tema: 'Tipos de saúde | Abordar a saúde física, mental e espiritual: diferenças e no que interferem.'},
-        {data: new Date(2024, 4, 30), responsavel: 'FERIADO', tema: 'Corpus Christi.'},
+        // {data: new Date(2024, 4, 9), responsavel: 'Pastoral', tema: 'Família - Dia das Mães | Fantoche com a temática: Família Buscapé.'},
+        // {data: new Date(2024, 4, 16), responsavel: 'SOE e SOP', tema: 'Mérito Acadêmico | Projeto de Vida.'},
+        // {data: new Date(2024, 4, 23), responsavel: 'Pastoral', tema: 'Tipos de saúde | Abordar a saúde física, mental e espiritual: diferenças e no que interferem.'},
+        // {data: new Date(2024, 4, 30), responsavel: 'FERIADO', tema: 'Corpus Christi.'},
 
-        {data: new Date(2024, 5, 6), responsavel: 'Pastoral', tema: 'Lançamento Campanhas Solidárias: Inverno Amigo e Mutirão de Natal.'},
-        {data: new Date(2024, 5, 13), responsavel: 'Coord. Disciplinar', tema: 'Orientações Disciplinares: O Código de Ética.'},
-        {data: new Date(2024, 5, 20), responsavel: 'Vice-direção', tema: 'Cultura Oriental/Japonesa | Imigrantes japoneses: Influência em nossa vida, alimentação e costumes. Por que são tão longevos?'},
-        {data: new Date(2024, 5, 27), responsavel: 'Pastoral', tema: 'Encerramento de ciclos | Animação RED: a vida é uma fera.'},
-        {data: new Date(2024, 6, 25), responsavel: 'SOE e SOP', tema: 'Mérito Acadêmico | Premiação.'},
-        {data: new Date(2024, 7, 1), responsavel: 'SOE e SOP', tema: 'Hábitos de Estudo | DIGA: Habilidade social anti-bullying.'},
+        // {data: new Date(2024, 5, 6), responsavel: 'Pastoral', tema: 'Lançamento Campanhas Solidárias: Inverno Amigo e Mutirão de Natal.'},
+        // {data: new Date(2024, 5, 13), responsavel: 'Coord. Disciplinar', tema: 'Orientações Disciplinares: O Código de Ética.'},
+        // {data: new Date(2024, 5, 20), responsavel: 'Vice-direção', tema: 'Cultura Oriental/Japonesa | Imigrantes japoneses: Influência em nossa vida, alimentação e costumes. Por que são tão longevos?'},
+        // {data: new Date(2024, 5, 27), responsavel: 'Pastoral', tema: 'Encerramento de ciclos | Animação RED: a vida é uma fera.'},
+        // {data: new Date(2024, 6, 25), responsavel: 'SOE e SOP', tema: 'Mérito Acadêmico | Premiação.'},
+        // {data: new Date(2024, 7, 1), responsavel: 'SOE e SOP', tema: 'Hábitos de Estudo | DIGA: Habilidade social anti-bullying.'},
 
+        //Agosto
+        {data: new Date(2024, 7, 1), responsavel: 'SOE e SOP', tema: 'Hábitos de Estudo | Motivação Intrínseca e Extrínseca.'},
+        {data: new Date(2024, 7, 8), responsavel: 'Pastoral', tema: 'Valor do pai terrestre e do Pai Celestial.'},
+        {data: new Date(2024, 7, 15), responsavel: 'SOE e SOP', tema: 'Estante Mágica | Apresentação do projeto de escrita do livro.'},
+        {data: new Date(2024, 7, 22), responsavel: 'SOE e SOP', tema: 'Lançamento projeto caixa de atividades | Enriquecimento curricular'},
+        {data: new Date(2024, 7, 29), responsavel: 'Direção', tema: 'Motivação | Lançamento de Matrículas.'},
+
+        //Setembro
+        {data: new Date(2024, 8, 5), responsavel: 'Pastoral', tema: 'Semana de Oração | Convidado especial: Pr Samuel Marcon.'},
+        {data: new Date(2024, 8, 12), responsavel: 'SOE e SOP', tema: 'Quebrando o Silêncio/Setembro Amarelo | Convidada especial: Psicóloga Stephany.'},
+        {data: new Date(2024, 8, 19), responsavel: 'SOE e SOP', tema: 'Alimentação saudável | Entrega dos presentes dos aniversariantes - Turmas do 3ºEF.'},
+        {data: new Date(2024, 8, 26), responsavel: 'SOE e SOP', tema: 'Semana de Oração | Convidado especial: Pr Samuel Marcon.'},
+
+        //Outubro
+        {data: new Date(2024, 9, 3), responsavel: 'SOE e SOP', tema: 'Dia mundial do sorriso | Convidado especial: Dentista Matheus Tosta (entrevistado pelo Pastor).'},
+        {data: new Date(2024, 9, 10), responsavel: 'SOE e SOP', tema: 'Semana da Criança | Atividades especiais.'},
+        {data: new Date(2024, 9, 17), responsavel: 'Pastoral', tema: 'Dia do Pastor | Entrevista de alunos com o Pr Vinicius.'},
+        {data: new Date(2024, 9, 24), responsavel: 'Pastoral', tema: 'Semana Criacionista | Carreta Hussein: exposição de animais da Savana Africana.'},
+        {data: new Date(2024, 9, 31), responsavel: 'SOE e SOP', tema: 'Motivação | Comemoração dos aniversariantes.'},
         
+        //Novembro
+        {data: new Date(2024, 10, 7), responsavel: 'Pastoral', tema: 'Cordêro Show | Conhecendo seu Diretor: entrevista com o Diretor.'},
+        {data: new Date(2024, 10, 14), responsavel: 'SOE e SOP', tema: 'Reunião Participativa de Pais | não haverá Cultura Geral.'},
+        {data: new Date(2024, 10, 21), responsavel: 'Pastoral e SOE', tema: 'Fechamentos e Mérito Acadêmico | Red PIXAR.'},
+        {data: new Date(2024, 10, 28), responsavel: 'SOE e SOP', tema: 'Thanksgiving e Dia do Monitor | Ação de graças e homenagem aos Monitores.'},
+
+        //Dezembro
+        {data: new Date(2024, 11, 5), responsavel: 'Pastoral e SOE', tema: 'Fechamentos e Mérito Acadêmico | Red PIXAR.'},
+
     ]
     
     celebras.forEach((e)=>{
