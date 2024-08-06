6b33a34aba9f38dcd1d0cf5eac51c41abb3e8151
Douglas <75459253+dls76@users.noreply.github.com>
2024-08-05 21:08:29 -0300

    prox eventos
    add beto carrero
    add pag externa beto

--- regressiva.js
+++ regressiva.js
@@ -15,14 +15,13 @@ const proximosEventos = [
   {nome: 'Fair Play Conhecimento', data: new Date(2024, 4, 14), link:"https://drive.google.com/drive/folders/1_-Hm088-AikWoHf8ETETAJZmTISF1_Qq?usp=drive_link",},
   {nome: 'Dia das Mães', data: new Date(2024, 4, 19), link:"https://drive.google.com/drive/folders/1vyONKagq4wUlMKRDGutaHqidhr3WFrbC?usp=drive_link",},
   {nome: 'Acreditação', data: new Date(2024, 4, 22), link:"https://dls76.github.io/dbox2/pg-externas/acreditacao.html",},
-  {nome: 'Olimpíadas do CAC', data: new Date(2024, 7, 21), link:"https://drive.google.com/drive/folders/1MeOeRl-HrD7_X-N-oTQcBAbROWVfKdOF?usp=sharing",},
+  {nome: 'Recesso', data: new Date(2024, 5, 29)},
   {nome: 'Lançamento de Matrículas Equipe Gestora', data: new Date(2024, 7, 12), link:"https://drive.google.com/drive/folders/1fAj27bIfdN-ZQTvadISFZq9J2H79YaTY?usp=sharing",},
   {nome: 'Programa Dia dos Pais', data: new Date(2024, 7, 18), link:"https://drive.google.com/drive/folders/1l_iYVD7GBGqXRdcaHT21lfocT4yC5DJP?usp=drive_link",},
-  {nome: 'Semana de Oração - 2º Semestre', data: new Date(2024, 7, 19), link:"https://drive.google.com/drive/folders/1wTuD_Njt9p-qmFbH07cnNBSAAG38z5ZQ?usp=drive_link",},
+  // {nome: 'Abertura Olimpíadas CAC', data: new Date(2024, 7, 20), link:"https://drive.google.com/drive/folders/1MeOeRl-HrD7_X-N-oTQcBAbROWVfKdOF?usp=sharing",},
+  {nome: 'Beto Carrero', data: new Date(2024, 7, 28), link: 'http://www.google.com'},
   {nome: 'Formaturas 9º e 3ºEM', data: new Date(2024, 11, 15), link:"https://drive.google.com/drive/folders/1JFtpr7N_WGLJnh37ETSgS9EJw-Y3nweu?usp=sharing",},
   {nome: 'Formatura Ed. Infantil', data: new Date(2024, 11, 17), link:"https://drive.google.com/drive/folders/1e8EjN1-wI4VhjStq0h0tCPN5GjRFkdvI?usp=sharing",},
-  {nome: 'Dia do Estudante', data: new Date(2024, 7, 11), link: ""},
-  {nome: 'Recesso', data: new Date(2024, 5, 29)},
 ];
 
 proximosEventos.sort((a, b) => a.data - b.data)
