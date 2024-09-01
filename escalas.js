8c93133f8d5eb2da8941e831fc16826e9f8e334e
Douglas <75459253+dls76@users.noreply.github.com>
2024-09-01 15:36:09 -0300

    escalas e horarios
    removed cultura geral de escalas e add em horarios

--- escalas.js
+++ escalas.js
@@ -178,7 +178,7 @@ let reuniaoadm = [
     {data: new Date(2024, 7, 8), link: 'https://docs.google.com/document/d/1mZIlLV_RcyJ-WzSJKOW2mUuw0THU2kBh-tKG9uNfcTM/edit?usp=sharing'},
     {data: new Date(2024, 7, 19), link: 'https://docs.google.com/document/d/1vi2-oLJOnA0Ko0VpoinHeLUd8aIf55LT8szn2-aryhA/edit', hora: '15:30'},
     {data: new Date(2024, 7, 26), link: 'https://docs.google.com/document/d/1PJ5WCP-m0p0XCJi7r-XbaXs3KaBnpOUz5YlYr-yYl9M/edit?usp=sharing', hora: '14:00'},
-    {data: new Date(2024, 8, 2), link: 'https://docs.google.com/document/d/101rrvsGBze6ZwlcTTUzNR9Kava4ayN0X83rzQPHnZJo/edit?usp=sharing', hora: '14:00'},
+    {data: new Date(2024, 8, 2), link: 'https://docs.google.com/document/d/101rrvsGBze6ZwlcTTUzNR9Kava4ayN0X83rzQPHnZJo/edit?usp=sharing', hora: '16:00'},
     {data: new Date(2024, 8, 9), link: 'https://docs.google.com/document/d/1IuDlLRka2kBM21Ra2h263ERRMyqvJz681nI_w5MnOck/edit?usp=sharing', hora: '14:00'},
     {data: new Date(2024, 8, 16), link: 'https://docs.google.com/document/d/13iCohiX1efAtCym0RSQEAvrDi84Aq4OSiFyFLMMwciE/edit?usp=drive_link', hora: '14:00'},
     {data: new Date(2024, 8, 23), link: 'https://docs.google.com/document/d/1djB2GlKVCPU28vMJDYQ-y9yGh1zMCONEMC5mztamMQo/edit?usp=drive_link', hora: '14:00'},
