895d798d6c6c6771a0706498b685608e41a003db
Douglas Leal <cac.diretoria@cace.asp.org.br>
2024-09-04 19:50:54 -0300

    calendario dbox
    add set e  out

--- calendario-dbox.js
+++ calendario-dbox.js
@@ -1,31 +1,79 @@
 const eventos = [
-    {nome: 'Evento 1', dataInicial: new Date(2024, 2, 1), dataFinal: new Date(2024, 2, 1), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 2', dataInicial: new Date(2024, 2, 2), dataFinal: new Date(2024, 2, 2), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 3', dataInicial: new Date(2024, 2, 3), dataFinal: new Date(2024, 2, 3), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 4', dataInicial: new Date(2024, 2, 4), dataFinal: new Date(2024, 2, 4), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 5', dataInicial: new Date(2024, 2, 5), dataFinal: new Date(2024, 2, 5), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 6', dataInicial: new Date(2024, 2, 6), dataFinal: new Date(2024, 2, 6), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 7', dataInicial: new Date(2024, 2, 7), dataFinal: new Date(2024, 2, 20), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 8', dataInicial: new Date(2024, 2, 8), dataFinal: new Date(2024, 2, 8), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 9', dataInicial: new Date(2024, 2, 9), dataFinal: new Date(2024, 2, 9), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 10', dataInicial: new Date(2024, 2, 10), dataFinal: new Date(2024, 2, 10), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 11', dataInicial: new Date(2024, 2, 11), dataFinal: new Date(2024, 2, 11), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 12', dataInicial: new Date(2024, 2, 12), dataFinal: new Date(2024, 2, 12), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 13', dataInicial: new Date(2024, 2, 13), dataFinal: new Date(2024, 2, 19), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 14', dataInicial: new Date(2024, 2, 14), dataFinal: new Date(2024, 2, 22), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 15', dataInicial: new Date(2024, 2, 15), dataFinal: new Date(2024, 2, 15), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 16', dataInicial: new Date(2024, 2, 16), dataFinal: new Date(2024, 2, 16), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 17', dataInicial: new Date(2024, 2, 17), dataFinal: new Date(2024, 2, 17), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 18', dataInicial: new Date(2024, 2, 18), dataFinal: new Date(2024, 2, 18), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 19', dataInicial: new Date(2024, 2, 19), dataFinal: new Date(2024, 2, 19), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento 20', dataInicial: new Date(2024, 2, 20), dataFinal: new Date(2024, 2, 21), responsavel: 'Douglas', foto: 'img/dls.png'},
+    
+    
+    {dataInicial: new Date(2024, 8, 2), dataFinal: new Date(2024, 8, 2), nome: "Reunião Administrativa - Departamentais e Coordenadores – Remoto (tarde)", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 2), dataFinal: new Date(2024, 8, 30), nome: "Hipótese de Escrita EI 4, 5 anos e 1º ano EF – 3ª Testagem", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 2), dataFinal: new Date(2024, 8, 2), nome: "Início do Ranking matrículas 2024", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 2), dataFinal: new Date(2024, 8, 2), nome: "Início Campanha de Matrículas 2025 – Digital", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 3), dataFinal: new Date(2024, 8, 3), nome: "Projetos Quebrando o Silêncio e de Sexualidade - FI", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 3), dataFinal: new Date(2024, 8, 3), nome: "Comissão de Educação/Diretiva", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 7), dataFinal: new Date(2024, 8, 7), nome: "Feriado Independência do Brasil", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 8), dataFinal: new Date(2024, 8, 8), nome: "Feriado Municipal (Curitiba) ", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 9), dataFinal: new Date(2024, 8, 13), nome: "Período de aplicação da AV2 – datas unificadas USB com fund 2 e EM - 2ª, 3ª E 4ª aula", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 10), dataFinal: new Date(2024, 8, 11), nome: "11 – Planejamento ASP", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 11), dataFinal: new Date(2024, 8, 15), nome: "I Will Go Teen – Encontro de Comunidades Memi – FAP", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 15), dataFinal: new Date(2024, 8, 18), nome: "Excursão de formandos 9ºEF (a confirmar)", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 15), dataFinal: new Date(2024, 8, 15), nome: "Plantão de Matrículas", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 17), dataFinal: new Date(2024, 8, 17), nome: "Formadores de Área – Tarde", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 17), dataFinal: new Date(2024, 8, 17), nome: "Dia D (primeiro)", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 17), dataFinal: new Date(2024, 8, 17), nome: "Fórum IATec – Financeiro – 15h", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 18), dataFinal: new Date(2024, 8, 18), nome: "Conselho de Classe (antes da recuperação)", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 20), dataFinal: new Date(2024, 8, 20), nome: "Entrega das convocações para a Recuperação Bimestral aos alunos", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 20), dataFinal: new Date(2024, 8, 20), nome: "Data limite para a entrega ao SOP dos conteúdos e métodos avaliativos do 4º bimestre", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 22), dataFinal: new Date(2024, 8, 22), nome: "Dia do Contador e Tesoureiro(a) escolar", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 23), dataFinal: new Date(2024, 8, 27), nome: "Recuperação Bimestral", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 23), dataFinal: new Date(2024, 8, 27), nome: "Aplicação das Provas Diagnósticas de Alfabetização ASP", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 23), dataFinal: new Date(2024, 9, 4),  nome: "Enem Interativo – aplicação", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 24), dataFinal: new Date(2024, 8, 24), nome: "Treinamento IATec – Adconnetc – TI’s", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 28), dataFinal: new Date(2024, 9, 4),  nome: "Excursão de Formandos 3ºEM (Caldas Novas - GO)", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 28), dataFinal: new Date(2024, 8, 28), nome: "Batismo da Primavera - ASP", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 30), dataFinal: new Date(2024, 8, 30), nome: "Final do 3º bimestre", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 30), dataFinal: new Date(2024, 8, 30), nome: "Dia da Secretária(o) ", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 30), dataFinal: new Date(2024, 8, 30), nome: "Data limite para o envio dos Cronogramas Avaliativos aos alunos", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 30), dataFinal: new Date(2024, 8, 30), nome: "Data limite para a regularização dos registros dos conteúdos do mês, no SAD", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 1), dataFinal: new Date(2024, 8, 30), nome: "Pesquisa de qualidade - Pais e responsáveis  - USB", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 1), dataFinal: new Date(2024, 8, 30), nome: "Análise do Calendário 2025", responsável: "Douglas", foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 8, 1), dataFinal: new Date(2024, 8, 30), nome: "Ensaio fotográfico – Formatura", responsável: "Douglas", foto: 'img/dls.png'},
 
-    // Outro calendário
-    {nome: 'Evento A', dataInicial: new Date(2024, 1, 1), dataFinal: new Date(2024, 2, 1), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento B', dataInicial: new Date(2024, 1, 2), dataFinal: new Date(2024, 2, 2), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento C', dataInicial: new Date(2024, 1, 3), dataFinal: new Date(2024, 2, 3), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento D', dataInicial: new Date(2024, 1, 4), dataFinal: new Date(2024, 2, 4), responsavel: 'Douglas', foto: 'img/dls.png'},
-    {nome: 'Evento E', dataInicial: new Date(2024, 1, 5), dataFinal: new Date(2024, 2, 5), responsavel: 'Douglas', foto: 'img/dls.png'},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Maná"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Encerramento da visitação dos alunos"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Treinamento IATec – e-Bolsa – Gestor de Bolsa"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Início do 4º bimestre"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 2), nome: "GEPEA Administrativo, Financeiro e Pedagógico - Presencial – FAP"},
+    {dataInicial: new Date(2024, 9, 2), dataFinal: new Date(2024, 9, 2), nome: "Culto do Envio – Pedagogia FAP"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Comissão de Educação/Diretiva"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Aulão Unificado - Terceirão "},
+    {dataInicial: new Date(2024, 9, 3), dataFinal: new Date(2024, 9, 5), nome: "Encontro MEMI/PR (FAP) - Pastor escolar + 2 ou 3 alunos"},
+    {dataInicial: new Date(2024, 9, 5), dataFinal: new Date(2024, 9, 5), nome: "Final Bom de Bíblia – ASP "},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 9, 7), nome: "Feriado Municipal Paranaguá) "},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 9, 11), nome: "Semana da Criança"},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 9, 7), nome: "Aula de Campo TMA (EI-5º | Manhã: 7 às 12; Tarde: 13h às 18h)"},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 9, 9), nome: "Fair Play KIDS CAC"},
+    {dataInicial: new Date(2024, 9, 10), dataFinal: new Date(2024, 9, 10), nome: "Aula de Campo Tio Miro (EI AO 1º ANO)"},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 9, 9), nome: "“Projeto Fair Play” - Módulo Jogos da Amizade "},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 9, 11), nome: "Comemoração do Dia do Professor por escola (combinar com profs compartilhados para participarem das escolas onde dá aula – presente recebe da escola onde tem o registro)"},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 9, 7), nome: "Reunião Administrativa - Departamentais e Coordenadores – Remoto (tarde)"},
+    {dataInicial: new Date(2024, 9, 7), dataFinal: new Date(2024, 10, 11), nome: "Estágio Pedagogia/FAP, nas unidades escolares"},
+    {dataInicial: new Date(2024, 9, 9), dataFinal: new Date(2024, 9, 9), nome: "Início Avaliações Cliente Oculto"},
+    {dataInicial: new Date(2024, 9, 11), dataFinal: new Date(2024, 9, 11), nome: "Data limite para entrega das avaliações 4º Bimestre - Para Análise do SOP "},
+    {dataInicial: new Date(2024, 9, 12), dataFinal: new Date(2024, 9, 12), nome: "Feriado N. S. Aparecida"},
+    {dataInicial: new Date(2024, 9, 14), dataFinal: new Date(2024, 9, 14), nome: "Recesso (dia permutado com recesso)"},
+    {dataInicial: new Date(2024, 9, 15), dataFinal: new Date(2024, 9, 15), nome: "Recesso – Dia do Professor "},
+    {dataInicial: new Date(2024, 9, 15), dataFinal: new Date(2024, 9, 15), nome: "Fórum IATec – Financeiro – 15h"},
+    {dataInicial: new Date(2024, 9, 16), dataFinal: new Date(2024, 9, 17), nome: "16 ou 17 - Aulão Unificado - Terceirão "},
+    {dataInicial: new Date(2024, 9, 18), dataFinal: new Date(2024, 9, 24), nome: "Análise de quadro de pessoal das unidades para 2024"},
+    {dataInicial: new Date(2024, 9, 18), dataFinal: new Date(2024, 9, 18), nome: "Dia do Pastor Escolar "},
+    {dataInicial: new Date(2024, 9, 19), dataFinal: new Date(2024, 9, 19), nome: "Matrículas abertas para alunos novos – vaga garantida para o aluno veterano até esse dia"},
+    {dataInicial: new Date(2024, 9, 19), dataFinal: new Date(2024, 9, 19), nome: "Dia do Técnico em Informática"},
+    {dataInicial: new Date(2024, 9, 24), dataFinal: new Date(2024, 9, 31), nome: "Período de aplicação da AV1 – datas unificadas para fund 2 e EM"},
+    {dataInicial: new Date(2024, 9, 21), dataFinal: new Date(2024, 9, 25), nome: "Semana Criacionista"},
+    {dataInicial: new Date(2024, 9, 26), dataFinal: new Date(2024, 9, 26), nome: "Sábado de Educação e Criacionismo"},
+    {dataInicial: new Date(2024, 9, 28), dataFinal: new Date(2024, 10, 11), nome: "PAAEB"},
+    {dataInicial: new Date(2024, 9, 29), dataFinal: new Date(2024, 9, 29), nome: "Capacitação Pastores de Escola: ACP e ASP"}, 
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Projetos “Rumo ao 6º ano” e “Rumo ao Ensino Médio” com os alunos"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Atenção aos Processos Seletivos Seriados das Universidades Estaduais: UEM, UEPG, UEL e UNICENTRO. (PAC, PAS e PSS)"},
+    {dataInicial: new Date(2024, 9, 1), dataFinal: new Date(2024, 9, 1), nome: "Atenção às inscrições dos vestibulares"},
+
 ]
 
 let currentDate = new Date()
@@ -46,10 +94,15 @@ eventosOrdenados.forEach((e) => {
     const data = document.createElement('div');
     data.classList.add('data');
 
-    if (e.dataInicial.getDate() !== e.dataFinal.getDate() || e.dataInicial.getMonth() !== e.dataFinal.getMonth()) {
-        data.innerText = `${e.dataInicial.getDate()} ${meses[e.dataInicial.getMonth()]} a ${e.dataFinal.getDate()} ${meses[e.dataFinal.getMonth()]}`;
+    let di = e.dataInicial.getDate()
+    let df = e.dataFinal.getDate()
+    let mi = e.dataInicial.getMonth()
+    let mf = e.dataFinal.getMonth()
+
+    if (di !== df || mi !== mf) {
+        data.innerText = `${di} a ${df} ${meses[mf]}`;
     } else {
-        data.innerText = `${e.dataInicial.getDate()} ${meses[e.dataInicial.getMonth()]}`;
+        data.innerText = `${di} ${meses[mi]}`;
     }
 
     const titulo = document.createElement('div');
