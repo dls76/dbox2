bf6b6f8fce7d6b6815be853b15ab7941c3f9a85a
Douglas <75459253+dls76@users.noreply.github.com>
2025-03-28 11:41:36 -0300

    add pedagogico

---
+++ pedagogico.js
@@ -0,0 +1,9 @@
+// Seleciona todos os botões com a classe "vermais"
+const buttons = document.querySelectorAll('.vermais');
+
+// Adiciona um ouvinte de evento de clique a cada botão
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Em construção');
+    });
+});
