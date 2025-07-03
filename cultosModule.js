b9f78df5c1ced635c7ec161e634518d0b0950997
dls76 <75459253+dls76@users.noreply.github.com>
2025-07-03 10:44:54 -0300

    cultos
    corrigido data atual

--- cultosModule.js
+++ cultosModule.js
@@ -18,7 +18,7 @@ export const escalaQuintaPersonalizada = {
 };
 
 export function getDataAtual() {
-  return new Date(2025, 5, 26);
+  return new Date();
 }
 
 export const horarios = ["7", "10", "13"];
