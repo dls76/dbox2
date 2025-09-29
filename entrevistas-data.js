b4e2724ce7cd95ea7f8192a17bf5289e50293bb5
dls76 <75459253+dls76@users.noreply.github.com>
2025-09-29 18:20:36 -0300

    escalas
    nova pagina de entrevistas

---
+++ entrevistas-data.js
@@ -0,0 +1,14 @@
+const entrevistasPadrao = [
+  "Jaque e Carol",        // segunda
+  "Valéria e Pr. Vini",   // terça
+  "Murilo e Joice",       // quarta
+  "Diretor e Camila",     // quinta
+  "Renata e Andressa"     // sexta
+];
+
+const alteracoes = [
+  { data: "2025-10-02", substituto: "Diretor e Andressa" }, 
+  { data: "2025-10-03", substituto: "Renata e Camila" }
+];
+
+export { entrevistasPadrao, alteracoes };
