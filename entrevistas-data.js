0930d1b370f826ce94968d40ea17f2d3c9d0b715
Douglas <75459253+dls76@users.noreply.github.com>
2025-10-20 20:34:22 -0300

    escalas
    entrevistas da semana - alteracao

--- entrevistas-data.js
+++ entrevistas-data.js
@@ -7,8 +7,10 @@ const entrevistasPadrao = [
 ];
 
 const alteracoes = [
-  { data: "2025-10-02", substituto: "Diretor e Andressa" }, 
-  { data: "2025-10-03", substituto: "Renata e Camila" }
+  // { data: "2025-10-02", substituto: "Diretor e Andressa" }, 
+  // { data: "2025-10-03", substituto: "Renata e Camila" },
+  { data: "2025-10-20", substituto: "Jaque e Renata" },
+  { data: "2025-10-24", substituto: "Carol e Andressa" }
 ];
 
 export { entrevistasPadrao, alteracoes };
