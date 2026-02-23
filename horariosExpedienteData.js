4a6d93186b9b28d1b5cbbf2210f8a2dafe9f2c33
dls76 <75459253+dls76@users.noreply.github.com>
2026-02-23 17:28:12 -0300

    adm
    introduciong horarios de trabalho

---
+++ horariosExpedienteData.js
@@ -0,0 +1,16 @@
+// data.js
+export const horarios = [
+  { nome: "Diretor",   entrada: "7:00",  intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+  { nome: "Murilo",    entrada: "8:00",  intervalo: "12:00", retorno: "13:00", saida: "18:00" },
+  { nome: "Carin",     entrada: "7:00",  intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+  { nome: "Josi",      entrada: "7:00",  intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+  { nome: "Rocha",     entrada: "7:00",  intervalo: "13:30", retorno: "14:30", saida: "17:00" },
+  { nome: "Pastor",    entrada: "7:00",  intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+  { nome: "Camila",    entrada: "7:00",  intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+  { nome: "Jaque",     entrada: "7:00",  intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+  { nome: "Carol",     entrada: "7:00",  intervalo: "12:30", retorno: "13:30", saida: "17:00" },
+  { nome: "Valeria",   entrada: "8:00",  intervalo: "12:00", retorno: "13:00", saida: "18:00" },
+  { nome: "Joyce",     entrada: "07:30", intervalo: "12:00", retorno: "13:00", saida: "17:30" },
+  { nome: "Andressa",  entrada: "8:00",  intervalo: "12:30", retorno: "13:30", saida: "18:00" },
+  { nome: "Renata",    entrada: "7:00",  intervalo: "12:00", retorno: "14:00", saida: "18:00" }
+];
