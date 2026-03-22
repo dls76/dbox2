5b886ea88c816a411f3bdf92981e198f48978c48
Douglas <75459253+dls76@users.noreply.github.com>
2026-03-22 10:02:02 -0300

    expediente

--- horariosExpedienteData.js
+++ horariosExpedienteData.js
@@ -4,19 +4,19 @@ export const setores = [
     label: "Administrativo",
     icon: "🏢",
     colaboradores: [
-      { nome: "Diretor",  entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Murilo",   entrada: "08:00", intervalo: "12:00", retorno: "13:00", saida: "18:00" },
-      { nome: "Carin",    entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Josi",     entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Rocha",    entrada: "07:00", intervalo: "13:30", retorno: "14:30", saida: "17:00" },
-      { nome: "Pastor",   entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Camila",   entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Jaque",    entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Carol",    entrada: "07:00", intervalo: "12:30", retorno: "13:30", saida: "17:00" },
-      { nome: "Valeria",  entrada: "08:00", intervalo: "12:00", retorno: "13:00", saida: "18:00" },
-      { nome: "Joyce",    entrada: "07:30", intervalo: "12:00", retorno: "13:00", saida: "17:30" },
+      { nome: "Diretor", entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+      { nome: "Murilo", entrada: "08:00", intervalo: "12:00", retorno: "13:00", saida: "18:00" },
+      { nome: "Carin", entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+      { nome: "Josi", entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+      { nome: "Rocha", entrada: "07:00", intervalo: "13:30", retorno: "14:30", saida: "17:00" },
+      { nome: "Pastor", entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+      { nome: "Camila", entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+      { nome: "Jaque", entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
+      { nome: "Carol", entrada: "07:00", intervalo: "12:30", retorno: "13:30", saida: "17:00" },
+      { nome: "Valeria", entrada: "08:00", intervalo: "12:00", retorno: "13:00", saida: "18:00" },
+      { nome: "Joyce", entrada: "07:30", intervalo: "12:00", retorno: "13:00", saida: "17:30" },
       { nome: "Andressa", entrada: "08:00", intervalo: "12:30", retorno: "13:30", saida: "18:00" },
-      { nome: "Renata",   entrada: "07:00", intervalo: "12:00", retorno: "14:00", saida: "18:00" }
+      { nome: "Renata", entrada: "07:00", intervalo: "12:00", retorno: "14:00", saida: "18:00" }
     ]
   },
   {
@@ -24,16 +24,19 @@ export const setores = [
     label: "Monitoria",
     icon: "🎧",
     colaboradores: [
-      { nome: "Bruno",    entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Leticia",  entrada: "08:00", intervalo: "12:00", retorno: "13:00", saida: "18:00" },
-      { nome: "Rafael",   entrada: "09:00", intervalo: "13:00", retorno: "14:00", saida: "18:00" },
-      { nome: "Tatiane",  entrada: "07:00", intervalo: "11:30", retorno: "12:30", saida: "16:30" },
-      { nome: "Diego",    entrada: "08:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Priscila", entrada: "07:30", intervalo: "12:00", retorno: "13:00", saida: "17:30" },
-      { nome: "Henrique", entrada: "09:00", intervalo: "13:30", retorno: "14:30", saida: "18:30" },
-      { nome: "Sabrina",  entrada: "07:00", intervalo: "12:00", retorno: "13:00", saida: "17:00" },
-      { nome: "Thiago",   entrada: "08:30", intervalo: "12:30", retorno: "13:30", saida: "18:30" },
-      { nome: "Monique",  entrada: "07:00", intervalo: "11:00", retorno: "12:00", saida: "16:00" }
+      { nome: "Tiago", entrada: "08:00", intervalo: "13:30", retorno: "14:30", saida: "18:00" },
+      { nome: "Kadu", entrada: "07:00", intervalo: "10:00", retorno: "12:00", saida: "18:00" },
+      { nome: "Yasmilly", entrada: "07:00", intervalo: "12:00", retorno: "14:00", saida: "18:00" },
+      { nome: "Luiz", entrada: "08:30", intervalo: "13:30", retorno: "15:30", saida: "19:30" },
+      { nome: "Matheus", entrada: "07:00", intervalo: "12:00", retorno: "14:00", saida: "18:00" },
+      { nome: "Patrícia", entrada: "09:00", intervalo: "12:00", retorno: "13:00", saida: "19:00" },
+      { nome: "Carmelis", entrada: "08:00", intervalo: "11:00", retorno: "13:00", saida: "18:00" },
+      { nome: "Lúcia", entrada: "06:45", intervalo: "11:00", retorno: "13:00", saida: "17:45" },
+      { nome: "Daniel", entrada: "06:30", intervalo: "10:45", retorno: "12:45", saida: "17:30" },
+      { nome: "Douglas", entrada: "08:00", intervalo: "13:30", retorno: "14:30", saida: "18:00" },
+      { nome: "Rose", entrada: "08:00", intervalo: "11:00", retorno: "12:00", saida: "18:00" },
+      { nome: "Eliane", entrada: "07:00", intervalo: "13:00", retorno: "15:00", saida: "18:00" },
     ]
+
   }
 ];
