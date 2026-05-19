2df5325e8c8a903f222d53122828ef7bcca742aa
Douglas <75459253+dls76@users.noreply.github.com>
2026-05-19 09:04:14 -0300

    RA

--- escalas.js
+++ escalas.js
@@ -10,6 +10,8 @@ const reuniaoadm = [
    { data: new Date(2026, 3, 14), link: 'https://docs.google.com/document/d/164VSrU54Ktkts5xt78ZdDDAGkaVfXa_Iag9PF-Rnakg/edit?usp=sharing', hora: '14h' },    
    { data: new Date(2026, 3, 28), link: 'https://docs.google.com/document/d/1nMJrHYbU49xTcwt1Mp0NEYfRCOirWLR3AmxVZU0KDTs/edit?usp=sharing', hora: '14h' },    
    { data: new Date(2026, 4, 5), link: 'https://docs.google.com/document/d/1Khq64vJihFPhwNfg98CTAXJqwua7QTBeI50f33-yu08/edit?usp=sharing', hora: '14h' },    
+   { data: new Date(2026, 4, 19), link: 'https://docs.google.com/document/d/1OwHQr8JevD1cNPvKIqoRshKF0dWEAlGBzzYqi58dibI/edit?usp=sharing', hora: '14h' },    
+   { data: new Date(2026, 4, 26), link: '', hora: '14h' },    
 ];
 
 const diaDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
