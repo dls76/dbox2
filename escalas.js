6325aa5a91ce767a15fc54ce635aea33a97e35c9
Douglas <75459253+dls76@users.noreply.github.com>
2026-06-29 16:22:43 -0300

    RA

--- escalas.js
+++ escalas.js
@@ -15,7 +15,7 @@ const reuniaoadm = [
    { data: new Date(2026, 5, 9), link: 'https://docs.google.com/document/d/1cLfKM4rqJOCJsYaZfZ6rd-aeOI712cQJeUn8my3TX9s/edit?usp=sharing', hora: '09h' },    
    //{ data: new Date(2026, 5, 16), link: 'https://docs.google.com/document/d/1zLb1ECeTgzRWT6yKAAhFcPO916iZ8walZ3C0GIYcNPc/edit?usp=sharing', hora: '14h' },    
    { data: new Date(2026, 5, 23), link: 'https://docs.google.com/document/d/1rUFNaNw2dkKHQ24kAR1MVq56GKKuci5fb5PcgbEAj8U/edit?usp=sharing', hora: '14h' },    
-   { data: new Date(2026, 5, 30), link: 'https://docs.google.com/document/d/1iGvoBk-pFjyVFACSqEOD0RjEW3lswN1DXKw2K_j_e-g/edit?usp=sharing', hora: '14h' },    
+   { data: new Date(2026, 5, 30), link: 'https://docs.google.com/document/d/1iGvoBk-pFjyVFACSqEOD0RjEW3lswN1DXKw2K_j_e-g/edit?usp=sharing', hora: '08h' },    
 ];
 
 const diaDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
